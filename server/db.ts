49b19ceaaa029166892fe547c47b28c59773fb42
Mrinalini Sinha <mrinalinisinha05@gmail.com>
2025-12-27 12:32:36 -0700

    Logging on error

--- server/db.ts
+++ server/db.ts
@@ -5,9 +5,10 @@ dotenv.config()
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 })
 
-pool.on('error', (err) => {
+pool.on('error', (err: Error) => {
   console.error('Unexpected error on idle client', err)
   process.exit(-1)
 })
